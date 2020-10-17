32b424245f11714b9fc0ccc9681fc5c1815d736f
darienmiller88 <darienmiller88@yahoo.com>
2020-10-17 15:56:01 -0400

    lab finished

--- zip-search/src/App.js
+++ zip-search/src/App.js
@@ -1,27 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import { Card, CardHeader, CardBody, CardText } from 'reactstrap';
 
 function City(props) {
-  return (<div>This is the City component</div>);
+  return (
+    <Card className = "city">
+      <CardHeader>{props.result.City}, {props.result.State}</CardHeader>
+      <CardBody>
+        <CardText>
+          <ul>
+            <li>State: {props.result.State}</li>
+            <li>Location: ({props.result.Lat}, {props.result.Long})</li>
+            <li>Population (estimated): {props.result.EstimatedPopulation}</li>
+            <li>Total Wages: ${props.result.TotalWages}</li>
+          </ul>     
+        </CardText>
+      </CardBody>
+    </Card>
+  );
 }
 
 function ZipSearchField(props) {
-  return (<div>This is the ZipSearchField component</div>);
+  return (
+    <div className="user-input">
+      <label>
+        <b>
+          Zip Code:
+        </b>
+      </label>
+      <input type="text" name="search" onChange={(e) => {props.setZip(e.target.value)}} placeholder="Enter Zipcode"></input>
+      <button onClick={ () => props.retrieveZipCode() }>Search</button>
+    </div>
+  );
 }
 
 
 class App extends Component {
+
+  state = {
+    results: [],
+    zip: ""
+  }
+
+  retrieveZipCode = async () => {
+    const response = await fetch(`http://ctp-zip-api.herokuapp.com/zip/` + this.state.zip);
+    if (response.ok) {
+      const json = await response.json();
+      this.setState({
+        results : json,
+      });
+    } else {
+      console.log("Error");
+    }
+  }
+
+  //Function to set zipcode
+  setZip = (zipcode) => {
+    this.setState({
+      zip : zipcode,
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Zip Code Search</h2>
         </div>
-        <ZipSearchField />
+
+        <ZipSearchField setZip = {this.setZip} retrieveZipCode = {this.retrieveZipCode}/>
         <div>
-          <City />
-          <City />
+          {
+            this.state.results.map(e => <City result={e}/>)
+          }
         </div>
       </div>
     );
